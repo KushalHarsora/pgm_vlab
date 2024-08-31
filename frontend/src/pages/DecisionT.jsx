c8feea14ef1bbeef560a4758239d5412e532a16d
YashAPro1 <rekhasd01000001@gmail.com>
2024-08-31 21:34:47 +0530

    Decision Tree Initial Commit

---
+++ frontend/src/pages/DecisionT.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DecisionT() {
+  return (
+    <div>
+      <h1>This is Decision Tree Page Don't you dare to touch this page!!</h1>
+    </div>
+  )
+}
+
+export default DecisionT
