a86ce2a32a70d17b4773744eb1844d744dac42f3
YashAPro1 <rekhasd01000001@gmail.com>
2024-08-31 21:44:59 +0530

    Started Working!!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DecisionT from "./pages/DecisionT";
 const App = () => {
   return (
-    <React.Fragment>
-      <main className=" h-screen w-screen flex justify-center items-center">
-        <p className=" text-5xl text-red-600 font-bold">
-          PGM Virtual Lab
-        </p>
-      </main>
-    </React.Fragment>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className="h-screen w-screen flex justify-center items-center">
+              <p className="text-5xl text-red-600 font-bold">PGM Virtual Lab</p>
+            </main>
+          }
+        />
+        <Route path="/deciT" element={<DecisionT />} />
+      </Routes>
+    </Router>
   )
 }
 
--- frontend/src/pages/DecisionT.jsx
+++ frontend/src/pages/DecisionT.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function DecisionT() {
   return (
-    <div>
-      <h1>This is Decision Tree Page Don't you dare to touch this page!!</h1>
+    <div className='h-screen w-screen '>
+        <div className='h-screen w-screen flex justify-center items-center'>
+
+      <h1 className='text-5xl text-blue-600 font-bold'>This is Decision Tree Page Don't you dare to touch this page!!</h1>
+        </div>
     </div>
   )
 }
